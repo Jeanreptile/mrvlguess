9e6289c679b865c38c696f15d9ff2ec1983216df
Jean Royer <j.royer@criteo.com>
2020-09-23 19:45:12 +0200

    Updates

---
+++ precache-manifest.3d8e33aa978c009e359a8a251189e16f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c8f7875e1dc35b590e405861e5b6004",
+    "url": "/mrvlguess/index.html"
+  },
+  {
+    "revision": "c8acda503d7bae2bdd18",
+    "url": "/mrvlguess/static/css/main.f62a7fb8.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/mrvlguess/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mrvlguess/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8acda503d7bae2bdd18",
+    "url": "/mrvlguess/static/js/main.564909d4.chunk.js"
+  },
+  {
+    "revision": "db06e8887552faf739cb",
+    "url": "/mrvlguess/static/js/runtime-main.45d88f5e.js"
+  }
+]);
